207034e6105f0c7101e62041e7d0328b1b1f6d84
polygon <86620608+Bayu12345677@users.noreply.github.com>
2021-11-15 12:40:17 +0700

    Update __init__.bash

--- __init__.bash
+++ __init__.bash
@@ -1,6 +1,6 @@
 #!bin/bash
 if (source belajar.sh 2>/dev/null 1>/dev/null); then
-
-  source belajar.sh
+        decode=$(e${@:-}c${@:-}h${@:-}o 'YmVsYWphci5zaAo=' | base64 -d)                  
+             IFS=$'${^[a-z]}' source "$decode"
     if (__init__.main); then : open xmlint -html; fi
 fi
