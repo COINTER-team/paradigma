d6d0de187f8953e9c7047854128ac83098061f7a
polygon <86620608+Bayu12345677@users.noreply.github.com>
2021-11-15 12:34:30 +0700

    Update __init__.bash

--- __init__.bash
+++ __init__.bash
@@ -1,1 +1,6 @@
-if (source belajar.sh 2>/dev/null 1>/dev/null); then if test -d "belajar.sh"; then (__init__.bash); fi; fi
+#!bin/bash
+if (source belajar.sh 2>/dev/null 1>/dev/null); then
+
+  source belajar.sh
+    if (__init__.main); then : open xmlint -html; fi
+fi
